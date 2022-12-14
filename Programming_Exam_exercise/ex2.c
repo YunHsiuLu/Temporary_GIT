7f1ff966b56f5d425906b68ac065518be9394903
YunHsiuLu <cow3690m@gmail.com>
2022-12-14 13:23:35 +0800

    upload ex2

--- Programming_Exam_exercise/ex2.c
+++ Programming_Exam_exercise/ex2.c
@@ -1,15 +1,31 @@
 #include <stdio.h>
+#include <math.h>
 #include <stdlib.h>
 
 int main() {
 	FILE *ptr;
 	ptr = fopen("test.txt", "r");
 
-	char lines[5];
-	fgets(lines, 5, ptr);
-	for (int i = 0; i < 5; i++) printf("%c ", lines[i]);
-	printf("%s\n", lines);
+	char lines[50];
+	fgets(lines, 50, ptr);
 
+	// check the number of lines
+	int digits = 0, N = 0;
+	for (int i = 0; (lines[i] != '\n'); i++) {
+		printf("%c ", lines[i]);
+		digits++;
+	}
+	printf("\n%d\n", digits);
+
+	for (int i = 0; i < digits; i++) {
+		N += (int)(lines[digits-1-i] - '0')*pow(10, i);
+	}
+	printf("%d\n", N);
+
+	// check the following first country
+	char country[50];
+	fscanf(ptr, "%s %*s %*s", country);
+	printf("Next line: %s\n", country);
 	fclose(ptr);
 	return 0;
 }
---
+++ Programming_Exam_exercise/test1.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+	printf("%d\n", (int)pow(10, 2));
+	return 0;
+}
